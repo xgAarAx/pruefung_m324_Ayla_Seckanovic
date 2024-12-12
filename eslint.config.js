7f4bdac8102dacd1b5eb15338033629e7603ec2d
xgAarAx <ayla.seckanovic@seantis.ch>
2024-12-12 09:28:46 +0100

    Revert "formatter test"
    This reverts commit bf6cc42454155aa71a761268920e2a956de4cdc5.

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,19 @@
 export default [
-  {
-    ignores:
-      [
-        'node_modules/',
-        'dist/',
-        'build/',
-      ], // Ignorierte Verzeichnisse
-  },
-  {
-    files:
-      [
-        '**/*.js',
-      ],
-    languageOptions:
-      {
-        ecmaVersion:
-          'latest',
-        sourceType:
-          'module',
-      },
-    rules:
-      {
-        semi: [
-          'error',
-          'always',
-        ],
-        quotes:
-          [
-            'error',
-            'double',
-          ],
-        'no-unused-vars':
-          'warn',
-        'prefer-const':
-          'warn',
-        eqeqeq:
-          'error',
-      },
-  },
+    {
+        ignores: ["node_modules/", "dist/", "build/"], // Ignorierte Verzeichnisse
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        rules: {
+            semi: ["error", "always"],
+            quotes: ["error", "double"],
+            "no-unused-vars": "warn",
+            "prefer-const": "warn",
+            "eqeqeq": "error",
+        },
+    },
 ];
