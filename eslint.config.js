bf6cc42454155aa71a761268920e2a956de4cdc5
xgAarAx <ayla.seckanovic@seantis.ch>
2024-12-12 09:27:25 +0100

    formatter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,41 @@
 export default [
-    {
-        ignores: ["node_modules/", "dist/", "build/"], // Ignorierte Verzeichnisse
-    },
-    {
-        files: ["**/*.js"],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-        },
-        rules: {
-            semi: ["error", "always"],
-            quotes: ["error", "double"],
-            "no-unused-vars": "warn",
-            "prefer-const": "warn",
-            "eqeqeq": "error",
-        },
-    },
+  {
+    ignores:
+      [
+        'node_modules/',
+        'dist/',
+        'build/',
+      ], // Ignorierte Verzeichnisse
+  },
+  {
+    files:
+      [
+        '**/*.js',
+      ],
+    languageOptions:
+      {
+        ecmaVersion:
+          'latest',
+        sourceType:
+          'module',
+      },
+    rules:
+      {
+        semi: [
+          'error',
+          'always',
+        ],
+        quotes:
+          [
+            'error',
+            'double',
+          ],
+        'no-unused-vars':
+          'warn',
+        'prefer-const':
+          'warn',
+        eqeqeq:
+          'error',
+      },
+  },
 ];
--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-import chalk from "chalk";
+import chalk from 'chalk';
 
-console.log(chalk.magenta("It works"));
-
+console.log(
+  chalk.magenta(
+    'It works',
+  ),
+);
 
 const myUnusedVariable = 5;
-console.log("Using myUnusedVariable:", myUnusedVariable);
-
+console.log(
+  'Using myUnusedVariable:',
+  myUnusedVariable,
+);
 
 const x = 10;
 
-
-if (x === 10) {
-    console.log("x is 10");
+if (
+  x === 10
+) {
+  console.log(
+    'x is 10',
+  );
 }
