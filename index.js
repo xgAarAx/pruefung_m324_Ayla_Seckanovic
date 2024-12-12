d250e3ede6bb32f10ae842cc183991734da178ac
xgAarAx <ayla.seckanovic@seantis.ch>
2024-12-12 09:25:28 +0100

    linter fixed

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import chalk from "chalk";
 
 console.log(chalk.magenta("It works"));
 
-let myUnusedVariable = 5;
 
-let x = 10;
-if (x == 10) {
+const myUnusedVariable = 5;
+console.log("Using myUnusedVariable:", myUnusedVariable);
+
+
+const x = 10;
+
+
+if (x === 10) {
     console.log("x is 10");
 }
